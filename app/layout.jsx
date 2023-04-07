6b5ecc0b1ee8630a708c3c2204b30ad81f5ec93f
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2023-04-06 18:53:17 -0700

    created movie file and delated about folder

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,11 @@
 import './globals.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  weights: ["400", "700"],
+  subsets: ['latin'],
+  variable: "--font-montserrat",
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,18 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <nav>
-          <h1>Logo</h1>
-          <ul>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Sign up</a>
-            </li>
-          </ul>
-        </nav>
+      <body className={`${montserrat.className} mx-32 my-12`}>
         {children}
       </body>
     </html>
